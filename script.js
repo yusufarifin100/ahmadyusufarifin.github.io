ad4918092f7e529e1f84d36c95f3240f479497d3
Ahmad Yusuf Arifin <35642419+yusufarifin100@users.noreply.github.com>
2025-04-28 07:13:52 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-const words = ["Your Name", "a Web3 Developer", "a Full-Stack Engineer"];
-let i = 0, j = 0, currentWord = '', isDeleting = false;
-const typedText = document.querySelector(".typed-text");
-
-function type() {
-    currentWord = words[i];
-    let display = currentWord.substring(0, j);
-    typedText.textContent = display;
-
-    if (!isDeleting && j < currentWord.length) {
-        j++;
-        setTimeout(type, 100);
-    } else if (isDeleting && j > 0) {
-        j--;
-        setTimeout(type, 50);
-    } else {
-        isDeleting = !isDeleting;
-        if (!isDeleting) i = (i + 1) % words.length;
-        setTimeout(type, 1000);
+const words = ["Ahmad Yusuf Arifin", "a Web Developer"];
+    let i = 0, j = 0, isDeleting = false;
+    const typedText = document.querySelector(".typed-text");
+    function type() {
+      let current = words[i];
+      typedText.textContent = current.substring(0, j);
+      if (!isDeleting && j < current.length) { j++; setTimeout(type, 100); }
+      else if (isDeleting && j > 0) { j--; setTimeout(type, 50); }
+      else { isDeleting = !isDeleting; if (!isDeleting) i = (i + 1) % words.length; setTimeout(type, 800); }
     }
-}
-
-document.addEventListener("DOMContentLoaded", type);
+    document.addEventListener("DOMContentLoaded", type);
